268edc17fcc9a81647af4e9343b9d2d6a5c2af8a
isaque <isaquesouzabraga@gmail.com>
2023-05-21 23:03:58 -0300

    inicial commit

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Modal, ActivityIndicator } from "react-native";
+import { AntDesign } from "@expo/vector-icons"
+import colors from "./Colors";
+import TodoList from "./components/TodoList";
+import AddListModel from "./components/AddListModal";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    addTodoVisible: false,
+    lists: [],
+    user: {},
+    loading: false
+  }
+
+  toogleAddTodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible })
+  }
+
+  renderList = list => {
+    return <TodoList list={list} updateList={this.updateList} />
+  }
+
+  addList = list => {
+    this.setState({ lists: [...this.state.lists, { ...list, id: this.state.lists.length + 1, todos: [] }] })
+  };
+
+  updateList = list => {
+    this.setState({
+      lists: this.state.lists.map(item => {
+        return item.id === list.id ? list : item
+      })
+    })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color={colors.blue} />
+        </View>
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toogleAddTodoModal()}>
+          <AddListModel closeModal={() => this.toogleAddTodoModal()} addList={this.addList} />
+        </Modal>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Lista de <Text style={{ fontWeight: "300", color: colors.blue }}>Tarefas</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList} onPress={() => this.toogleAddTodoModal()}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Adicionar Lista</Text>
+        </View>
+
+        <View style={{ height: 350, padding: 32 }}>
+          <FlatList
+            data={this.state.lists}
+            keyExtractor={item => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+            keyboardShouldPersistTaps="always" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
+  }
 });
---
+++ components/AddListModal.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default class AddListModel extends React.Component {
+    backgroundColors = ["#5CD859", "#24A6D9", "#595BD9", "#8022D9", "#D159D8", "#D85963", "#D88559"]
+
+    state = {
+        name: "",
+        color: this.backgroundColors[0]
+    };
+
+    createTodo = () => {
+        const { name, color } = this.state;
+
+        const list = { name, color };
+
+        this.props.addList(list);
+
+        this.setState({ name: "" });
+        this.props.closeModal();
+    }
+
+    renderColors() {
+        return this.backgroundColors.map(color => {
+            return (
+                <TouchableOpacity
+                    key={color}
+                    style={[styles.colorSelector, { backgroundColor: color }]}
+                    onPress={() => this.setState({ color })} />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <TouchableOpacity style={{ position: "absolute", top: 20, right: 32 }} onPress={this.props.closeModal}>
+                    <AntDesign name="close" size={24} color={colors.black} />
+                </TouchableOpacity>
+
+                <View style={{ alignSelf: "stretch", marginHorizontal: 32 }}>
+                    <Text style={styles.title}>Criar Lista de Tarefas</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nome da Lista"
+                        onChangeText={text => this.setState({ name: text })} />
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 12 }}>
+                        {this.renderColors()}
+                    </View>
+                    <TouchableOpacity
+                        style={[styles.create, { backgroundColor: this.state.color }]}
+                        onPress={this.createTodo}>
+                        <Text style={{ color: colors.white, fontWeight: "600" }}>CRIAR</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "800",
+        color: colors.black,
+        alignSelf: "center",
+        marginBottom: 16
+    },
+    input: {
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: colors.blue,
+        height: 50,
+        marginTop: 8,
+        paddingHorizontal: 16,
+        fontSize: 16
+    },
+    create: {
+        marginTop: 24,
+        height: 50,
+        borderRadius: 6,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    colorSelector: {
+        width: 30,
+        height: 30,
+        borderRadius: 4
+    }
+});
